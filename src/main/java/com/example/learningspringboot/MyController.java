f0b1ad66abb5fa19e0efbe0457236c1a3ed93668
Salman Malik <smalik@pivotal.io>
2022-07-12 16:16:03 -0400

    path variable

--- src/main/java/com/example/learningspringboot/MyController.java
+++ src/main/java/com/example/learningspringboot/MyController.java
@@ -1,17 +1,18 @@
 package com.example.learningspringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/greeting/{count}")
     public String greeting(
-            @RequestParam(name="m", defaultValue="hello world!") String message) {
-        return "message: " + message;
+            @RequestParam(name="m", defaultValue="hello world!") String message,
+            @PathVariable("count") int count) {
+        return "message: " + message + ", count: " + count;
     }
-
 
 }
