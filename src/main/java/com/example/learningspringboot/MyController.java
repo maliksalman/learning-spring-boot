a718ec7c20eaad88de819306ab03ae6c87375430
Salman Malik <smalik@pivotal.io>
2022-07-12 16:12:46 -0400

    request params

--- src/main/java/com/example/learningspringboot/MyController.java
+++ src/main/java/com/example/learningspringboot/MyController.java
@@ -1,14 +1,15 @@
 package com.example.learningspringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
     @GetMapping("/greeting")
-    public String greeting() {
-        return "message: hello world!!!";
+    public String greeting(@RequestParam("m") String message) {
+        return "message: " + message + "!!!";
     }
 
 
