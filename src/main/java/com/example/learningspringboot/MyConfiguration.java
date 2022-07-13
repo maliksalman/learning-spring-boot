7eb1eb17f526ac971aee9d0b990ab2e399c9c392
Salman Malik <smalik@pivotal.io>
2022-07-13 15:48:13 -0400

    Adding YML properties for defaults

--- src/main/java/com/example/learningspringboot/MyConfiguration.java
+++ src/main/java/com/example/learningspringboot/MyConfiguration.java
@@ -1,13 +1,8 @@
 package com.example.learningspringboot;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfiguration {
 
-    @Bean
-    public MyDefaults myDefault() {
-        return new MyDefaults(35, "chicago");
-    }
 }
--- src/main/java/com/example/learningspringboot/MyDefaults.java
+++ src/main/java/com/example/learningspringboot/MyDefaults.java
@@ -1,11 +1,20 @@
 package com.example.learningspringboot;
 
-import lombok.Value;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Value
+@Getter
+@Component
 public class MyDefaults {
 
     private int age;
     private String city;
 
+    public MyDefaults(
+            @Value("${defaults.age}") int age,
+            @Value("${defaults.city}") String city) {
+        this.age = age;
+        this.city = city;
+    }
 }
